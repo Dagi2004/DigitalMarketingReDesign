c17976be8a62aa32e31bd056c2d98761ad52fcc0
dagmawitesfay <dagmawi.milkias@spexsports.app>
2026-09-01 22:54:05 -0400

    made them work for upperCase using toUpperCase() method

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -4,7 +4,7 @@ import { Footer } from "../Resuable/Footer";
 
 import homehero from "../../assets/about-hero.png";
 import innovation from "../../assets/innovation.png";
-import integrity from "../../assets/Intergrity";
+import integrity from "../../assets/Intergrity.png";
 import growth from "../../assets/Growthh.png";
 import journey from "../../assets/growthjourney.png";
 
